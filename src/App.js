488aaf5b8ad77ca85110fdbfc9efccbd43991d13
shahalam sharif <shahalam.shareef@gmail.com>
2022-04-18 03:20:41 +0600

    ServiceCard component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner/Banner";
 import Nav from "./components/Nav/Nav";
+import ServiceCard from "./components/ServiceCard/ServiceCard";
 
 function App() {
   return (
     <div className="container">
       <Nav></Nav>
       <Banner></Banner>
+      <ServiceCard></ServiceCard>
     </div>
   );
 }
