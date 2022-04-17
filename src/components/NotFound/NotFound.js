719068815b4d70060a765cc6b9f1514bc85432fd
shahalam sharif <shahalam.shareef@gmail.com>
2022-04-18 03:12:03 +0600

    NotFound component done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        className="img-fluid mb-5"
+        src="https://i.ibb.co/d2NSNmx/404.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default NotFound;
